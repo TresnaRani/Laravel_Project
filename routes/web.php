4ab8ec2e4d8f0fb855aaf283601246766574aaf4
Tresna <52813382+TresnaRani@users.noreply.github.com>
2023-03-29 22:50:11 +0600

    web

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\siteController;
 
 
-Route::get('/', function () {
-    return view('HomePage');
-});
+Route::get('/', [siteController::class, 'Home'])->name('/');
 
-Route::get('/about', function () {
-    return view('AboutPage');
-});
+Route::get('/about', [siteController::class, 'About'])->name('/about');
 
-Route::get('/contact', function () {
-    return view('ContactPage');
-});
+Route::get('/contact', [siteController::class, 'Contact'])->name('/contact');
+
